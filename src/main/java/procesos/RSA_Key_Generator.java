700b3e82db2202b674922af29285e6350979b1d3
Albert Gonzalez <56592643+Albert-cmd@users.noreply.github.com>
2019-11-26 19:45:55 +0100

    Update RSA_Key_Generator.java

--- src/main/java/procesos/RSA_Key_Generator.java
+++ src/main/java/procesos/RSA_Key_Generator.java
@@ -5,10 +5,45 @@
  */
 package procesos;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
 /**
  *
  * @author Albert Gonzalez
  */
 public class RSA_Key_Generator {
     
+  
+
+public class RSAKeyPairGen {
+    
+    public  void GenerateRSAKeys (){
+        try {
+            
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(1024); // You can set a different value here
+            
+            KeyPair kp = kpg.generateKeyPair();
+            PrivateKey prk = kp.getPrivate();
+            PublicKey puk = kp.getPublic();
+            
+            Base64.Encoder enc = Base64.getEncoder();
+            
+            System.out.println();
+            System.out.println("---- ( RSA Key Pair Generator ) ----");
+            System.out.println("Private Key: " + enc.encodeToString(prk.getEncoded()));
+            System.out.println();
+            System.out.println("Public Key: " + enc.encodeToString(puk.getEncoded()));
+            System.out.println();
+            System.out.println("--- End ---");
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
 }
