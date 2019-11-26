1311945a962e2eb87c96e6901da0ff6d563ddee6
Albert Gonzalez <56592643+Albert-cmd@users.noreply.github.com>
2019-11-26 19:19:26 +0100

    Segundo Commit

---
+++ src/main/java/procesos/Hash_Generator_SHA256.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package procesos;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author Albert Gonzalez
+ */
+public class Hash_Generator_SHA256 {
+    
+
+    public void messageToBytes (String missatge) throws NoSuchAlgorithmException{
+    
+    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+    byte[] encodedhash = digest.digest(
+    missatge.getBytes(StandardCharsets.UTF_8));
+    
+    }
+    
+   
+    
+    private static String bytesToHex(byte[] hash) {
+        
+    StringBuffer hexString = new StringBuffer();
+    
+    for (int i = 0; i < hash.length; i++) {
+    String hex = Integer.toHexString(0xff & hash[i]);
+    if(hex.length() == 1) hexString.append('0');
+        hexString.append(hex);
+        
+    }
+    
+    return hexString.toString();
+}
+    
+    
+    
+    
+    
+    
+    
+}
